83c3d16ad3c26179e904bb08b832790c021cca4b
ahmedmunir <mryagamie@outlook.com>
2020-08-08 10:44:36 +0200

    Display Tweets at home page using AJAX request with basic CSS styles

--- tweets/views.py
+++ tweets/views.py
@@ -9,9 +9,26 @@ from tweets.models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+# List all Tweets
+def tweet_list_view(request, *args, **kwargs):
+    """
+        REST API View for all Tweets
+    """
+    query_set = Tweet.objects.all()
+    
+    # Convert tweets queryset into list to be able to send it through JSON
+    tweets = [{"id": tweet.id, "content": tweet.content} for tweet in query_set]
+    data = {
+        "isUser": False,
+        "tweets": tweets
+    }
+    return JsonResponse(data)
 
 # Tweet detail view
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+        REST API View for specific tweet 
+    """
     data = {
         "id": tweet_id
     }
