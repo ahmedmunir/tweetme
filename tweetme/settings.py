e3f89ae4a3447ecdb3003a468d07551817cffea9
ahmedmunir <mryagamie@outlook.com>
2020-08-17 01:36:19 +0200

    Modify allowed hosts of app

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://tweetmewebsite.herokuapp.com/']
+ALLOWED_HOSTS = ['tweetmewebsite.herokuapp.com']
 
 
 # Application definition
