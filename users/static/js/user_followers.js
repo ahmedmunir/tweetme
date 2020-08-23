a8600a4841aa5d600420f9f9053cd92112f571da
ahmedmunir <mryagamie@outlook.com>
2020-08-23 05:13:35 +0200

    Fix click follow & unfollow buttons on Following & followers html pages

--- users/static/js/user_followers.js
+++ users/static/js/user_followers.js
@@ -15,89 +15,54 @@ const getCookie =  name =>  {
     return cookieValue;
 }
 
-if(document.querySelector('.owner_follow_button')){
-    document.querySelector('.owner_follow_button').addEventListener('click', e => {
-        e.preventDefault();
-    
-        let url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/follow/`;
-        let data = new FormData();
-        let state = document.querySelector('.owner_follow_button').innerHTML;
-        let target = document.querySelector('.user_username').dataset.username; 
-        data.append('state', state);
-        data.append('target', target);
-    
-        fetch(url, {
-            method: 'POST',
-            body: data,
-    
-            headers: {
-                'X-CSRFToken': getCookie('csrftoken'),
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            
-            // If follow process succeded 
-            if(data['state'] == 'follow') {
-                document.querySelector('.owner_follow_button').classList.remove('follow');
-                document.querySelector('.owner_follow_button').classList.add('unfollow');
-                document.querySelector('.owner_follow_button').innerHTML = "Unfollow";
-
-                // Increase number of followers
-                document.querySelector('.followers').textContent =
-                parseInt(document.querySelector('.followers').textContent) + 1;
-            }
-    
-            // If unfollow process succeded
-            else if(data['state'] == 'unfollow') {
-                document.querySelector('.owner_follow_button').classList.add('follow');
-                document.querySelector('.owner_follow_button').classList.remove('unfollow');
-                document.querySelector('.owner_follow_button').innerHTML = "Follow"; 
-
-                // Decrease number of followers
-                document.querySelector('.followers').textContent =
-                parseInt(document.querySelector('.followers').textContent) - 1;
-            }
-        })
-    })
-}
 // Follow or UnFollow
 if(document.querySelector('.follow_button')){
-    document.querySelector('.follow_button').addEventListener('click', e => {
-        e.preventDefault();
+    document.querySelectorAll('.follow_button').forEach(follow_button => {
+        follow_button.addEventListener('click', e => {
+            e.preventDefault();
+        
+            let url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/follow/`;
+            let data = new FormData();
+            let state = document.querySelector('.follow_button').innerHTML;
+            let target = e.target.closest('.user_container').dataset.username;
+            data.append('state', state);
+            data.append('target', target);
+        
+            fetch(url, {
+                method: 'POST',
+                body: data,
+        
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken'),
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                
+                // If follow process succeded 
+                if(data['state'] == 'follow') {
+                    document.querySelector('.follow_button').classList.remove('follow');
+                    document.querySelector('.follow_button').classList.add('unfollow');
+                    document.querySelector('.follow_button').innerHTML = "Unfollow";
     
-        let url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/follow/`;
-        let data = new FormData();
-        let state = document.querySelector('.follow_button').innerHTML;
-        let target = e.target.closest('.user_container').dataset.username;
-        data.append('state', state);
-        data.append('target', target);
+                    // Increase number of followers
+                    document.querySelector('.followers').textContent =
+                    parseInt(document.querySelector('.followers').textContent) + 1;
+                }
+        
+                // If unfollow process succeded
+                else if(data['state'] == 'unfollow') {
+                    document.querySelector('.follow_button').classList.add('follow');
+                    document.querySelector('.follow_button').classList.remove('unfollow');
+                    document.querySelector('.follow_button').innerHTML = "Follow"; 
     
-        fetch(url, {
-            method: 'POST',
-            body: data,
-    
-            headers: {
-                'X-CSRFToken': getCookie('csrftoken'),
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            
-            // If follow process succeded 
-            if(data['state'] == 'follow') {
-                document.querySelector('.follow_button').classList.remove('follow');
-                document.querySelector('.follow_button').classList.add('unfollow');
-                document.querySelector('.follow_button').innerHTML = "Unfollow";
-            }
-    
-            // If unfollow process succeded
-            else if(data['state'] == 'unfollow') {
-                document.querySelector('.follow_button').classList.add('follow');
-                document.querySelector('.follow_button').classList.remove('unfollow');
-                document.querySelector('.follow_button').innerHTML = "Follow"; 
-
-            }
+                    // Decrease number of followers
+                    document.querySelector('.followers').textContent =
+                    parseInt(document.querySelector('.followers').textContent) - 1;
+                }
+            })
         })
     })
+
 }
+
